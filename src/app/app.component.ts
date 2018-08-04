df11231c455557486cf9a53ddc9a818f71eabbe0
disnex <disnex@users.noreply.github.com>
2018-08-04 13:24:41 +0200

    done..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   objectKeys = Object.keys;
   cryptoCurrencies: Currencies[];
   public cryptoValue: any;
+  public jedanNiz = [2,3,4];
+
+  drugiNiz = [2,3,4];
 
   moneyCurrencies: Currencies[];
   public moneyValue: any;
@@ -19,38 +22,64 @@ export class AppComponent {
   bomba: number;
   cryptos: any;
 
+  public vrijednost: number;
+
   
 
   constructor(private _data: DataService) { }
 
   ngOnInit(){
     this.money = 0;
+    this.vrijednost = 1;
 
     this.cryptoCurrencies = [
       {id: 0, name: 'Bitcoin (BTC)', value: 'BTC'},
       {id: 1, name: 'IOTA (IOT)', value: 'IOT'},
-      {id: 2, name: 'DASH (Dash)', value: 'DASH'}
+      {id: 2, name: 'Dash (DASH)', value: 'DASH'}
     ];
     this.cryptoValue = "BTC";
-    
+
+    this._data.getPrices(this.cryptoValue)
+      .subscribe(res => {
+        this.cryptos = res;
+        this.jedanNiz[0] = this.cryptos.USD;
+        this.jedanNiz[1] = this.cryptos.EUR;
+        this.jedanNiz[2] = this.cryptos.AUD;
+        console.log(this.jedanNiz);
+        this.drugiNiz[0] = this.jedanNiz[0] * this.vrijednost;
+        this.drugiNiz[1] = this.jedanNiz[1] * this.vrijednost;
+        this.drugiNiz[2] = this.jedanNiz[2] * this.vrijednost;
+      });
+
+    /*
     this.moneyCurrencies = [
       {id: 0, name: 'Australian Dollar (AUD)', value: 'AUD'},
       {id: 1, name: 'US Dollar (USD)', value: 'USD'},
       {id: 2, name: 'Euro (EUR)', value: 'EUR'}
     ];
     this.moneyValue = "USD";
+    */
   }
 
-  printSelected(vrijednost){
-    this._data.getPrices(this.cryptoValue, this.moneyValue)
+
+  
+  promjena(){
+    this.drugiNiz[0] = this.jedanNiz[0] * this.vrijednost;
+    this.drugiNiz[1] = this.jedanNiz[1] * this.vrijednost;
+    this.drugiNiz[2] = this.jedanNiz[2] * this.vrijednost;
+  }
+
+  izmjena(){
+    this._data.getPrices(this.cryptoValue)
       .subscribe(res => {
         this.cryptos = res;
-        let mon = this.moneyValue;
-        this.money = vrijednost * this.cryptos[this.moneyValue];
+        this.jedanNiz[0] = this.cryptos.USD;
+        this.jedanNiz[1] = this.cryptos.EUR;
+        this.jedanNiz[2] = this.cryptos.AUD;
+        console.log(this.jedanNiz);
+        this.drugiNiz[0] = this.jedanNiz[0] * this.vrijednost;
+        this.drugiNiz[1] = this.jedanNiz[1] * this.vrijednost;
+        this.drugiNiz[2] = this.jedanNiz[2] * this.vrijednost;
       });
-    
-   //console.log(this.cryptos[this.moneyValue]);
-    
   }
-  
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,8 +13,8 @@ export class DataService {
   constructor(private _http: HttpClient) { }
 
 
-  getPrices(crypto, money){
-    this._url = 'https://min-api.cryptocompare.com/data/price?fsym='+crypto+'&tsyms='+money;
+  getPrices(crypto){
+    this._url = 'https://min-api.cryptocompare.com/data/price?fsym='+crypto+'&tsyms=USD,EUR,AUD';
     return this._http.get(this._url).pipe(map(result => this.result = result));
     //return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms='+crypto+'&tsyms='+money").pipe(map(result => this.result = result));
   }
